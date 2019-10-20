9b48a63364e962616516d7dcb03f66772b3a8952
John Leskas <john.leskas@gmail.com>
2019-10-20 21:01:15 +0300

    📦 NEW: Implement footer social from Customizer options + FontAwesome

--- functions.php
+++ functions.php
@@ -336,6 +336,7 @@ class StarterSite extends Timber\Site {
 		wp_register_script( 'scriptjs', get_stylesheet_directory_uri() . '/static/script.js', array( 'jquery' ), get_version( get_stylesheet_directory_uri() . '/static/script.js' ), true );
 		wp_register_script( 'events', get_stylesheet_directory_uri() . '/static/events.js', array( 'jquery' ), get_version( get_stylesheet_directory_uri() . '/static/events.js' ), true );
 
+		wp_enqueue_style( 'fontawesome', '//stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', 'all' );
 		wp_enqueue_style( 'fonts', get_stylesheet_directory_uri() . '/static/fonts/fonts.css', false, get_version( '/fonts/fonts.css' ), 'all' );
 		wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/style.css', false, '1.0.0', 'all' );
 		wp_enqueue_style( 'custom', get_stylesheet_directory_uri() . '/css/custom.css', false, '1.0.0', 'all' );
--- index.php
+++ index.php
@@ -13,15 +13,27 @@
  * @since   Timber 0.1
  */
 
-$site_logo         = get_theme_mod( 'site_logo' );
-$footer_logo       = get_theme_mod( 'footer_logo' );
-$footer_copyrights = get_theme_mod( 'pogo_footer_copyrights' );
-$gtm_script_tag    = get_theme_mod( 'pogo_gtm_script' );
+$site_logo          = get_theme_mod( 'site_logo' );
+$footer_logo        = get_theme_mod( 'footer_logo' );
+$footer_copyrights  = get_theme_mod( 'pogo_footer_copyrights' );
+$gtm_script_tag     = get_theme_mod( 'pogo_gtm_script' );
+$show_footer_social = get_theme_mod( 'show_footer_social' ); // Show footer social checkbox
+$social_sites       = pogo_get_social_sites(); // Get all social sites as an array
+
+// Any inputs that aren't empty are stored in $social_networks array
+foreach ( $social_sites as $key => $social_site ) {
+	if ( strlen( get_theme_mod( $social_site ) ) > 0 ) {
+		$social_networks[ $key ]['name'] = $social_site;
+		$social_networks[ $key ]['url']  = get_theme_mod( $social_site );
+	}
+}
 
 $context                            = Timber::context();
 $context['posts']                   = new Timber\PostQuery();
 $context['site_logo']               = $site_logo;
 $context['footer_logo']             = $footer_logo;
+$context['show_footer_social']      = $show_footer_social;
+$context['social_networks']         = $social_networks;
 $context['footer_copyrights']       = $footer_copyrights;
 $context['footer_main_widget_area'] = Timber::get_widgets( 'footer_main_widget_area' );
 $templates                          = array( 'index.twig' );
