231e1f98e323bcdaa1bd73af62d38ffb89f69d90
John Leskas <john.leskas@gmail.com>
2019-11-05 00:05:57 +0200

    IMPROVE: Another set of improvements

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,36 +1,51 @@
 "use strict";
 
 jQuery(document).ready(function ($) {
-  // Select all links with hashes
-  jQuery('a[href*="#"]') // Remove links that don't actually link to anything
-  .not('[href="#"]').not('[href="#0"]').click(function (event) {
-    // On-page links
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      // Figure out element to scroll to
-      var target = jQuery(this.hash);
-      target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']'); // Does a scroll target exist?
 
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        jQuery('html, body').animate({
-          scrollTop: target.offset().top - 150
-        }, 1000, function () {
-          // Callback after animation
-          // Must change focus!
-          var jQuerytarget = jQuery(target);
-          jQuerytarget.focus();
+	var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 
-          if (jQuerytarget.is(":focus")) {
-            // Checking if the target was focused
-            return false;
-          } else {// jQuerytarget.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            // jQuerytarget.focus(); // Set focus again
-          }
 
-          ;
-        });
-      }
-    }
-  });
+	if(viewportWidth < 768) {
+		window.onscroll = function (e) {
+			if(window.scrollY > 10) {
+				jQuery('#header').addClass('sticky');
+			} else {
+				jQuery('#header').removeClass('sticky');
+			}
+		};
+	}
+
+
+	// Select all links with hashes
+	jQuery('a[href*="#"]') // Remove links that don't actually link to anything
+		.not('[href="#"]').not('[href="#0"]').click(function (event) {
+		// On-page links
+		if(location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			// Figure out element to scroll to
+			var target = jQuery(this.hash);
+			target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']'); // Does a scroll target exist?
+
+			if(target.length) {
+				// Only prevent default if animation is actually gonna happen
+				event.preventDefault();
+				jQuery('html, body').animate({
+					scrollTop: target.offset().top - 150
+				}, 1000, function () {
+					// Callback after animation
+					// Must change focus!
+					var jQuerytarget = jQuery(target);
+					jQuerytarget.focus();
+
+					if(jQuerytarget.is(":focus")) {
+						// Checking if the target was focused
+						return false;
+					} else {// jQuerytarget.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+						// jQuerytarget.focus(); // Set focus again
+					}
+
+					;
+				});
+			}
+		}
+	});
 }());
