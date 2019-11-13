9a89f75e050c606030ce621b4df2d51165f864de
John Leskas <john.leskas@gmail.com>
2019-11-13 18:11:43 +0200

    👌 IMPROVE: Small UI improvements

--- assets/js/custom/main.js
+++ assets/js/custom/main.js
@@ -20,7 +20,7 @@ jQuery(document).ready(function ($) {
 					// Only prevent default if animation is actually gonna happen
 					event.preventDefault();
 					jQuery('html, body').animate({
-						scrollTop: target.offset().top - 150
+						scrollTop: target.offset().top - 85
 					}, 1000, function () {
 						// Callback after animation
 						// Must change focus!
